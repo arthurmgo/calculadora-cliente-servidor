e29b4457a0aebb176718a7141983bde451c7d4b6
Arthur Gomes <arthur.mirandag@gmail.com>
2017-12-15 16:13:48 -0200

    update

--- src/ServidorOperacao/ServidorMultiplicacao.java
+++ src/ServidorOperacao/ServidorMultiplicacao.java
@@ -1,45 +1,19 @@
 package ServidorOperacao;
 
 import java.io.IOException;
-import java.io.PrintStream;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.util.Scanner;
 
-public class ServidorMultiplicacao {
-
-    private int porta;
+public class ServidorMultiplicacao extends ServidorOp {
 
     public ServidorMultiplicacao(int porta) {
-        this.porta = porta;
+        super(porta);
     }
-
-    public int multiplicacao(int x, int y){
-        return x * y;
-    }
-
-    public void executa () throws IOException {
-        ServerSocket servidor = new ServerSocket(this.porta);
-        System.out.println("Porta " + this.porta + " aberta");
-
-        while (true) {
-            // aceita um cliente
-            Socket cliente = servidor.accept();
-            Scanner s = new Scanner(cliente.getInputStream());
-            PrintStream ps = new PrintStream(cliente.getOutputStream());
-            int x = s.nextInt();
-            int y = s.nextInt();
-            int resp = multiplicacao(x,y);
-            ps.println(resp);
-
-
-        }
-
-    }
-
 
     public static void main(String[] args) throws IOException {
         new ServidorMultiplicacao(10003).executa();
     }
 
+    @Override
+    public int op(int a, int b) {
+        return a*b;
+    }
 }
